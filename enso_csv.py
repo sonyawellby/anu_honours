0e4ab1c3aa944c4aca1b9ac7fdb6c36e86f58c38
Sonya Wellby <u4680942@anu.edu.au>
2015-08-25 12:47:09 +1000

    Updated hadisst_prepare to include an extended version of dataset from jan 1900 to dec 2005 to compute ENSO running means/phases

--- enso_csv.py
+++ enso_csv.py
@@ -8,6 +8,11 @@ Last updated 25 August 2015.
 
 from enso import *
 
+from hadisst_prepare import sst_JanuaryEx,sst_FebruaryEx,sst_MarchEx,\
+     sst_AprilEx,sst_MayEx,sst_JuneEx,sst_JulyEx,sst_AugustEx,sst_SeptemberEx,\
+     sst_OctoberEx,sst_NovemberEx,sst_DecemberEx
+
+
 """
 from access_prepare_ts import ts_January,ts_February,ts_March,\
      ts_April,ts_May,ts_June,ts_July,ts_August,ts_September,\
@@ -18,7 +23,7 @@ from hadisst_prepare import sst_January,sst_February,sst_March,\
      sst_October,sst_November,sst_December
 """
 
-
+"""
 #test = [1,-1,3,4,5,6,7,8,9]
 test = [-2,-0.3,0,0.2,1,5,0.4,-0.4,10,5,6,5,3,2,0.1,-5]
 
@@ -29,7 +34,7 @@ run = cropRM(test1)
 
 ENSOphases = ENSOphase(test1,0,(len(test1)-6))
 (ENSOpos,ENSOneg,ENSOneutral) = ENSOphases
-
+"""
 
 #_____________________________________________    
 #from hadisst_prepare import sst_Annual
--- hadisst_prepare.py
+++ hadisst_prepare.py
@@ -2,7 +2,7 @@
 A set of routines to prepare HadISST1.1 sea-surface temperature data.
 
 Submitted by Sonya Wellby for ENVS4055, 2015.
-Last updated 21 August 2015.
+Last updated 25 August 2015.
 """
 
 import netCDF4 as n
@@ -26,7 +26,19 @@ def hadisstTrim():
     data_flat = np.ma.masked_less(data_flat,mask)
     return data_flat
 
-def bugFix():
+def hadisstTrimExt():
+    """
+    A function to read in the HadISST dataset and trim it to the period
+    January 1900 to December 2005.  For use in computing Nino3.4 running
+    means and ENSO phases (see enso.py).
+
+    Note: data_flat: months,latitude,longitude
+    """
+    data_flat_ext = data.variables['sst'][360:1632]
+    data_flat_ext = np.ma.masked_less(data_flat_ext,mask)
+    return data_flat_ext
+
+def bugFix(dataset,ext=False):
     """
     A function to mask values along the dateline from 1982 onwards
     (http://hadobs.metoffice.com/hadisst/ - see the comment on
@@ -34,12 +46,25 @@ def bugFix():
     and the date-line spans longitude [0] and longitude [359]
     (-179.5 E to 179.5 E).
     This affects computation of the IPO TPI index only (not Nino3.4).
+
+    Parameters:
+    -----------
+    dataset : Either data_flat or data_flat_ext
+    ext : (default = False)
+            If the dataset has been extended to Jan 1900-Dec 2005,
+            ext should be set to 'True'.  In this case, the timeslice
+            changes to accommodate for the extra five months at the
+            beginning of the extended dataset.
     """
-    b = data_flat[:]
-    b[984:,:,0:360:359] = -9999.0
+    b = dataset[:]
+    if ext==False:
+        b[984:,:,0:360:359] = -9999.0
+    else:
+        b[989:,:,0:360:359] = -9999.0
     dataFix = np.ma.masked_less_equal(b,-9999.0)
     return dataFix
 
+#Can only be used with 105 year data.
 def hadisstAnnual():
     """
     A function to convert flat data to an array with all 105 years
@@ -48,176 +73,370 @@ def hadisstAnnual():
     Note: hadisstData[year,month,lat,lon]
     e.g. hadisstData[104,11,179,359]
          = May 2005 at 180 deg N (89.5 deg) and 360 deg E (179.5 deg E)
+
+    NB: can currently only be used with 105 year data (e.g. data_flat).
     """
     data = np.reshape(dataFix,(105,12,180,360))
     return data
 
-def hadisstJune():
+def hadisstJune(years,start):
     """
     A function to produce an array of June data for all 105 years.
     Note: June[year,lat,lon]
+
+    Parameters:
+    -----------
+    years: the number of years recording this month.  If using
+            data_flat this will be 105, if using data_flat_ext
+            this will be 106.
+    start: the index to start slicing from (coinciding with the
+            first instance of this month).
+            For 105 years = 0
+            For 106 years = 5
     """
-    June = dataFix[0::12]
+    June = dataFix[start::12]
     June = np.ma.masked_less(June,mask)
-    data = np.reshape(June,(105,180,360))
+    data = np.reshape(June,(years,180,360))
     return June
 
-def hadisstJuly():
+def hadisstJuly(years,start):
     """
     A function to produce an array of July data for all 105 years.
     Note: July[year,lat,lon]
+    
+    Parameters:
+    -----------
+    years: the number of years recording this month.  If using
+            data_flat this will be 105, if using data_flat_ext
+            this will be 106.
+    start: the index to start slicing from (coinciding with the
+            first instance of this month).
+            For 105 years = 1
+            For 106 years = 6
     """
-    July = dataFix[1::12]
+    July = dataFix[start::12]
     July = np.ma.masked_less(July,mask)
-    data = np.reshape(July,(105,180,360))
+    data = np.reshape(July,(years,180,360))
     return July
 
-def hadisstAugust():
+def hadisstAugust(years,start):
     """
     A function to produce an array of August data for all 105 years.
     Note: August[year,lat,lon]
+    
+    Parameters:
+    -----------
+    years: the number of years recording this month.  If using
+            data_flat this will be 105, if using data_flat_ext
+            this will be 106.
+    start: the index to start slicing from (coinciding with the
+            first instance of this month).
+            For 105 years = 2
+            For 106 years = 7
     """
-    August = dataFix[2::12]
+    August = dataFix[start::12]
     August = np.ma.masked_less(August,mask)
-    data = np.reshape(August,(105,180,360))
+    data = np.reshape(August,(years,180,360))
     return August
 
-def hadisstSeptember():
+def hadisstSeptember(years,start):
     """
     A function to produce an array of September data for all 105 years.
     Note: September[year,lat,lon]
+    
+    Parameters:
+    -----------
+    years: the number of years recording this month.  If using
+            data_flat this will be 105, if using data_flat_ext
+            this will be 106.
+    start: the index to start slicing from (coinciding with the
+            first instance of this month).
+            For 105 years = 3
+            For 106 years = 8
     """
-    September = dataFix[3::12]
+    September = dataFix[start::12]
     September = np.ma.masked_less(September,mask)
-    data = np.reshape(September,(105,180,360))
+    data = np.reshape(September,(years,180,360))
     return September
 
-def hadisstOctober():
+def hadisstOctober(years,start):
     """
     A function to produce an array of October data for all 105 years.
     Note: October[year,lat,lon]
+    
+    Parameters:
+    -----------
+    years: the number of years recording this month.  If using
+            data_flat this will be 105, if using data_flat_ext
+            this will be 106.
+    start: the index to start slicing from (coinciding with the
+            first instance of this month).
+            For 105 years = 4
+            For 106 years = 9
     """
-    October = dataFix[4::12]
+    October = dataFix[start::12]
     October = np.ma.masked_less(October,mask)
-    data = np.reshape(October,(105,180,360))
+    data = np.reshape(October,(years,180,360))
     return October
 
-def hadisstNovember():
+def hadisstNovember(years,start):
     """
     A function to produce an array of November data for all 105 years.
     Note: November[year,lat,lon]
+    
+    Parameters:
+    -----------
+    years: the number of years recording this month.  If using
+            data_flat this will be 105, if using data_flat_ext
+            this will be 106.
+    start: the index to start slicing from (coinciding with the
+            first instance of this month).
+            For 105 years = 5
+            For 106 years = 10
     """
-    November = dataFix[5::12]
+    November = dataFix[start::12]
     November = np.ma.masked_less(November,mask)
-    data = np.reshape(November,(105,180,360))
+    data = np.reshape(November,(years,180,360))
     return November
 
-def hadisstDecember():
+def hadisstDecember(years,start):
     """
     A function to produce an array of December data for all 105 years.
     Note: December[year,lat,lon]
+    
+    Parameters:
+    -----------
+    years: the number of years recording this month.  If using
+            data_flat this will be 105, if using data_flat_ext
+            this will be 106.
+    start: the index to start slicing from (coinciding with the
+            first instance of this month).
+            For 105 years = 6
+            For 106 years = 11
     """
-    December = dataFix[6::12]
+    December = dataFix[start::12]
     December = np.ma.masked_less(December,mask)
-    data = np.reshape(December,(105,180,360))
+    data = np.reshape(December,(years,180,360))
     return December
 
-def hadisstJanuary():
+def hadisstJanuary(years,start):
     """
     A function to produce an array of January data for all 105 years.
     Note: January[year,lat,lon]
+    
+    Parameters:
+    -----------
+    years: the number of years recording this month.  If using
+            data_flat this will be 105, if using data_flat_ext
+            this will be 106.
+    start: the index to start slicing from (coinciding with the
+            first instance of this month).
+            For 105 years = 7
+            For 106 years = 0
     """
-    January = dataFix[7::12]
+    January = dataFix[start::12]
     January = np.ma.masked_less(January,mask)
-    data = np.reshape(January,(105,180,360))
+    data = np.reshape(January,(years,180,360))
     return January
 
-def hadisstFebruary():
+def hadisstFebruary(years,start):
     """
     A function to produce an array of Feburary data for all 105 years.
     Note: February[year,lat,lon]
+    
+    Parameters:
+    -----------
+    years: the number of years recording this month.  If using
+            data_flat this will be 105, if using data_flat_ext
+            this will be 106.
+    start: the index to start slicing from (coinciding with the
+            first instance of this month).
+            For 105 years = 8
+            For 106 years = 1
     """
-    February = dataFix[8::12]
+    February = dataFix[start::12]
     February = np.ma.masked_less(February,mask)
-    data = np.reshape(February,(105,180,360))
+    data = np.reshape(February,(years,180,360))
     return February
 
-def hadisstMarch():
+def hadisstMarch(years,start):
     """
     A function to produce an array of March data for all 105 years.
     Note: March[year,lat,lon]
+    
+    Parameters:
+    -----------
+    years: the number of years recording this month.  If using
+            data_flat this will be 105, if using data_flat_ext
+            this will be 106.
+    start: the index to start slicing from (coinciding with the
+            first instance of this month).
+            For 105 years = 9
+            For 106 years = 2
     """
-    March = dataFix[9::12]
+    March = dataFix[start::12]
     March = np.ma.masked_less(March,mask)
-    data = np.reshape(March,(105,180,360))
+    data = np.reshape(March,(years,180,360))
     return March
 
-def hadisstApril():
+def hadisstApril(years,start):
     """
     A function to produce an array of April data for all 105 years.
     Note: April[year,lat,lon]
+    
+    Parameters:
+    -----------
+    years: the number of years recording this month.  If using
+            data_flat this will be 105, if using data_flat_ext
+            this will be 106.
+    start: the index to start slicing from (coinciding with the
+            first instance of this month).
+            For 105 years = 10
+            For 106 years = 3
     """
-    April = dataFix[10::12]
+    April = dataFix[start::12]
     April = np.ma.masked_less(April,mask)
-    data = np.reshape(April,(105,180,360))
+    data = np.reshape(April,(years,180,360))
     return April
 
-def hadisstMay():
+def hadisstMay(years,start):
     """
     A function to produce an array of May data for all 105 years.
     Note: May[year,lat,lon]
+    
+    Parameters:
+    -----------
+    years: the number of years recording this month.  If using
+            data_flat this will be 105, if using data_flat_ext
+            this will be 106.
+    start: the index to start slicing from (coinciding with the
+            first instance of this month).
+            For 105 years = 11
+            For 106 years = 4
     """
-    May = dataFix[11::12]
+    May = dataFix[start::12]
     May = np.ma.masked_less(May,mask)
-    data = np.reshape(May,(105,180,360))
+    data = np.reshape(May,(years,180,360))
     return May
 
-def hadisstJJA():
+def hadisstJJA(years,start1,start2,start3):
     """
     A function to produce an array of JJA seasonal data (average of
     all three months at each location) for all 105 years.
     Note: JJA[year,lat,lon]
+
+    Parameters:
+    -----------
+    years: the number of years recording this seasons.  If using
+            data_flat this will be 105, if using data_flat_ext
+            this will be 106.
+    start1: the index to start slicing from for the first month in
+            this season (coinciding with the first instance of this month).
+            For 105 years = 0
+            For 106 years = 5
+    start2: the index to start slicing from for the first month in
+            this season (coinciding with the first instance of this month).
+            For 105 years = 1
+            For 106 years = 6
+    start3: the index to start slicing from for the first month in
+            this season (coinciding with the first instance of this month).
+            For 105 years = 2
+            For 106 years = 7
     """
-    JJA_flat = dataFix[0::12] + data_flat[1::12] + data_flat[2::12]
+    JJA_flat = dataFix[start1::12] + dataFix[start2::12] + dataFix[start3::12]
     JJA_flat /= 3.0
     JJA = np.ma.masked_less(JJA_flat,mask)
-    data = np.reshape(JJA,(105,180,360))
+    data = np.reshape(JJA,(years,180,360))
     return JJA
 
-def hadisstSON():
+def hadisstSON(years,start1,start2,start3):
     """
     A function to produce an array of SON seasonal data (average of
     all three months at each location) for all 105 years.
     Note: SON[year,lat,lon]
+
+    Parameters:
+    -----------
+    years: the number of years recording this seasons.  If using
+            data_flat this will be 105, if using data_flat_ext
+            this will be 106.
+    start1: the index to start slicing from for the first month in
+            this season (coinciding with the first instance of this month).
+            For 105 years = 3
+            For 106 years = 8
+    start2: the index to start slicing from for the first month in
+            this season (coinciding with the first instance of this month).
+            For 105 years = 4
+            For 106 years = 9
+    start3: the index to start slicing from for the first month in
+            this season (coinciding with the first instance of this month).
+            For 105 years = 5
+            For 106 years = 10
     """
-    SON_flat = dataFix[3::12] + data_flat[4::12] + data_flat[5::12]
+    SON_flat = dataFix[start1::12] + dataFix[start2::12] + dataFix[start3::12]
     SON_flat /= 3.0
     SON = np.ma.masked_less(SON_flat,mask)
-    data = np.reshape(SON,(105,180,360))
+    data = np.reshape(SON,(years,180,360))
     return SON
 
-def hadisstDJF():
+def hadisstDJF(years,start1,start2,start3):
     """
     A function to produce an array of DJF seasonal data (average of
     all three months at each location) for all 105 years.
     Note: DJF[year,lat,lon]
+
+    Parameters:
+    -----------
+    years: the number of years recording this seasons.  If using
+            data_flat this will be 105, if using data_flat_ext
+            this will be 106.
+    start1: the index to start slicing from for the first month in
+            this season (coinciding with the first instance of this month).
+            For 105 years = 6
+            For 106 years = 11
+    start2: the index to start slicing from for the first month in
+            this season (coinciding with the first instance of this month).
+            For 105 years = 7
+            For 106 years = 0
+    start3: the index to start slicing from for the first month in
+            this season (coinciding with the first instance of this month).
+            For 105 years = 8
+            For 106 years = 1
     """
-    DJF_flat = dataFix[6::12] + data_flat[7::12] + data_flat[8::12]
+    DJF_flat = dataFix[start1::12] + dataFix[start2::12] + dataFix[start3::12]
     DJF_flat /= 3.0
     DJF = np.ma.masked_less(DJF_flat,mask)
-    data = np.reshape(DJF,(105,180,360))
+    data = np.reshape(DJF,(years,180,360))
     return DJF
 
-def hadisstMAM():
+def hadisstMAM(years,start1,start2,start3):
     """
     A function to produce an array of MAM seasonal data (average of
     all three months at each location) for all 105 years.
     Note: MAM[year,lat,lon]
+
+    Parameters:
+    -----------
+    years: the number of years recording this seasons.  If using
+            data_flat this will be 105, if using data_flat_ext
+            this will be 106.
+    start1: the index to start slicing from for the first month in
+            this season (coinciding with the first instance of this month).
+            For 105 years = 9
+            For 106 years = 2
+    start2: the index to start slicing from for the first month in
+            this season (coinciding with the first instance of this month).
+            For 105 years = 10
+            For 106 years = 3
+    start3: the index to start slicing from for the first month in
+            this season (coinciding with the first instance of this month).
+            For 105 years = 11
+            For 106 years = 4
     """
-    MAM_flat = dataFix[9::12] + data_flat[10::12] + data_flat[11::12]
+    MAM_flat = dataFix[start1::12] + dataFix[start2::12] + dataFix[start3::12]
     MAM_flat /= 3.0
     MAM = np.ma.masked_less(MAM_flat,mask)
-    data = np.reshape(MAM,(105,180,360))
+    data = np.reshape(MAM,(years,180,360))
     return MAM
 
 #Make lat/lon data accessible for use in other files
@@ -229,27 +448,51 @@ lonHad_units = data.variables['longitude'].units
 #Prepare data for analysis
 mask = -10.0 #Mask values less than -10 deg Celsius to remove no-data values of -1e+30
 data_flat = hadisstTrim()
-dataFix = bugFix()
+data_flat_ext = hadisstTrimExt()
 
-#Divide into time bins
+
+#Divide into time bins - June 1900 to May 2005
+dataFix = bugFix(data_flat)
 
 sst_Annual = hadisstAnnual()
 
-sst_June = hadisstJune()
-sst_July = hadisstJuly()
-sst_August = hadisstAugust()
-sst_September = hadisstSeptember()
-sst_October = hadisstOctober()
-sst_November = hadisstNovember()
-sst_December = hadisstDecember()
-sst_January = hadisstJanuary()
-sst_February = hadisstFebruary()
-sst_March = hadisstMarch()
-sst_April = hadisstApril()
-sst_May = hadisstMay()
+sst_June = hadisstJune(105,0)
+sst_July = hadisstJuly(105,1)
+sst_August = hadisstAugust(105,2)
+sst_September = hadisstSeptember(105,3)
+sst_October = hadisstOctober(105,4)
+sst_November = hadisstNovember(105,5)
+sst_December = hadisstDecember(105,6)
+sst_January = hadisstJanuary(105,7)
+sst_February = hadisstFebruary(105,8)
+sst_March = hadisstMarch(105,9)
+sst_April = hadisstApril(105,10)
+sst_May = hadisstMay(105,11)
 
-sst_JJA = hadisstJJA()
-sst_SON = hadisstSON()
-sst_DJF = hadisstDJF()
-sst_MAM = hadisstMAM()
+sst_JJA = hadisstJJA(105,0,1,2)
+sst_SON = hadisstSON(105,3,4,5)
+sst_DJF = hadisstDJF(105,6,7,8)
+sst_MAM = hadisstMAM(105,9,10,11)
+
+
+#Divide into time bins - January 1900 to December 2005
+dataFix = bugFix(data_flat_ext,ext=True)
+
+sst_JuneEx = hadisstJune(106,5)
+sst_JulyEx = hadisstJuly(106,6)
+sst_AugustEx = hadisstAugust(106,7)
+sst_SeptemberEx = hadisstSeptember(106,8)
+sst_OctoberEx = hadisstOctober(106,9)
+sst_NovemberEx = hadisstNovember(106,10)
+sst_DecemberEx = hadisstDecember(106,11)
+sst_JanuaryEx = hadisstJanuary(106,0)
+sst_FebruaryEx = hadisstFebruary(106,1)
+sst_MarchEx = hadisstMarch(106,2)
+sst_AprilEx = hadisstApril(106,3)
+sst_MayEx = hadisstMay(106,4)
+
+sst_JJAEx = hadisstJJA(106,5,6,7)
+sst_SONEx = hadisstSON(106,8,9,10)
+sst_DJFEx = hadisstDJF(106,11,0,1)
+sst_MAMEx = hadisstMAM(106,2,3,4)
 
