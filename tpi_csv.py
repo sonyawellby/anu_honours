8de1d67858a5e70a4040e092a089f9f691b605a8
Sonya Wellby <u4680942@anu.edu.au>
2015-08-27 13:02:17 +1000

    Various changes

--- tpi_csv.py
+++ tpi_csv.py
@@ -164,7 +164,7 @@ neutralR3 = IPOneutral
 
 
 """
-#Write filtered and unfiltered data to CSV files
+Write filtered and unfiltered data to CSV files
 """
 output = np.column_stack((Had_monthsTPI_uf.flatten(),Had_monthsTPI.flatten(),\
                           Acc_monthsTPI_uf_r1.flatten(),Acc_monthsTPI_r1.flatten(),\
